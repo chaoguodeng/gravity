606f46fcf16ffc94f8ede580c1379cda7699e8da
sammy00 <xiangminli@sjtu.edu.cn>
2018-04-20 12:57:48 +0800

    add the missing test file for package ec

---
+++ crypto/ec/ec_test.go
@@ -0,0 +1,41 @@
+package ec_test
+
+import (
+	"crypto/rand"
+	"testing"
+
+	"github.com/sammy00/gravity/crypto/ec"
+	"github.com/sammy00/gravity/crypto/ec/ecdsa"
+	"github.com/sammy00/gravity/crypto/ec/ed25519"
+	"golang.org/x/crypto/sha3"
+)
+
+func runWorker(worker ec.Worker, t *testing.T) {
+	priv, pub, err := worker.GenerateKey(rand.Reader)
+	if nil != err {
+		t.Fatal(err)
+	}
+
+	rawMsg := []byte("Hello World")
+	digest := sha3.Sum256(rawMsg)
+
+	sig, err := worker.Sign(priv, digest[:])
+	if nil != err {
+		t.Fatal(err)
+	}
+
+	if !worker.Verify(pub, digest[:], sig) {
+		t.Fatal("the verification shouldn't fail")
+	}
+}
+
+func TestWorker(t *testing.T) {
+	ed25519Worker := new(ed25519.Worker)
+	runWorker(ed25519Worker, t)
+
+	ecdsa256Worker := new(ecdsa.Worker256)
+	runWorker(ecdsa256Worker, t)
+
+	ecdsa512Worker := new(ecdsa.Worker512)
+	runWorker(ecdsa512Worker, t)
+}
